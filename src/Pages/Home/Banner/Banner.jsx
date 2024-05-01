0ac462cb8adbc816973cffa5529582cf442aee6d
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-05-01 11:43:20 +0600

    added about section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -11,7 +11,7 @@ export const Banner = () => {
     <div className="carousel w-full h-[600px]">
   <div id="slide1" className="carousel-item relative w-full ">
     <img src={img1} alt='banner' className="w-full rounded-xl" />
-    <div className="absolute h-full flex items-center space-x-4 left-0 top-0 bg-gradient-to-r from-[#151515] to-[#15151500]">
+    <div className="absolute rounded-xl h-full flex items-center space-x-4 left-0 top-0 bg-gradient-to-r from-[#151515] to-[#15151500]">
      <div className='text-white pl-12 space-y-4'>
         <h1 className='text-5xl font-bold w-1/3'>Affordable Price For Car Servicing</h1>
         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
@@ -27,8 +27,8 @@ export const Banner = () => {
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src={img2} alt='banner' className="w-full" />
-    <div className="absolute h-full flex items-center space-x-4 left-0 top-0 bg-gradient-to-r from-[#151515] to-[#15151500]">
+    <img src={img2} alt='banner' className="w-full rounded-xl" />
+    <div className="absolute rounded-xl h-full flex items-center space-x-4 left-0 top-0 bg-gradient-to-r from-[#151515] to-[#15151500]">
      <div className='text-white pl-12 space-y-4'>
         <h1 className='text-5xl font-bold w-1/3'>Affordable Price For Car Servicing</h1>
         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
@@ -43,9 +43,9 @@ export const Banner = () => {
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
-  <div id="slide3" className="carousel-item relative w-full">
+  <div id="slide3" className="carousel-item relative w-full rounded-xl">
     <img src={img3} alt='banner' className="w-full" />
-    <div className="absolute h-full flex items-center space-x-4 left-0 top-0 bg-gradient-to-r from-[#151515] to-[#15151500]">
+    <div className="absolute rounded-xl h-full flex items-center space-x-4 left-0 top-0 bg-gradient-to-r from-[#151515] to-[#15151500]">
      <div className='text-white pl-12 space-y-4'>
         <h1 className='text-5xl font-bold w-1/3'>Affordable Price For Car Servicing</h1>
         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
@@ -60,9 +60,9 @@ export const Banner = () => {
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div> 
-  <div id="slide4" className="carousel-item relative w-full">
+  <div id="slide4" className="carousel-item relative w-full rounded-xl">
     <img src={img4} alt='banner' className="w-full" />
-    <div className="absolute h-full flex items-center space-x-4 left-0 top-0 bg-gradient-to-r from-[#151515] to-[#15151500]">
+    <div className="absolute h-full rounded-xl flex items-center space-x-4 left-0 top-0 bg-gradient-to-r from-[#151515] to-[#15151500]">
      <div className='text-white pl-12 space-y-4'>
         <h1 className='text-5xl font-bold w-1/3'>Affordable Price For Car Servicing</h1>
         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { About } from "./About/About"
 import { Banner } from "./Banner/Banner"
 
 export const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <About></About>
     </div>
   )
 }
